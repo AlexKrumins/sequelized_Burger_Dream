6b23270803a6e6f0c4477afc9e62e7ed15d0313c
Alex Krumins <justkrumins@gmail.com>
2019-06-01 18:22:18 -0500

    chef api routes set

---
+++ routes/chef-api-routes.js
@@ -0,0 +1,39 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/chefs", function(req, res) {
+    db.Chef.findAll({
+      include: [db.Post]
+    }).then(function(dbChef) {
+      res.json(dbChef);
+    });
+  });
+
+  app.get("/api/chefs/:id", function(req, res) {
+    db.Chef.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Post]
+    }).then(function(dbChef) {
+      res.json(dbChef);
+    });
+  });
+
+  app.post("/api/chefs", function(req, res) {
+    db.Chef.create(req.body).then(function(dbChef) {
+      res.json(dbChef);
+    });
+  });
+
+  app.delete("/api/chefs/:id", function(req, res) {
+    db.Chef.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbChef) {
+      res.json(dbChef);
+    });
+  });
+
+};
