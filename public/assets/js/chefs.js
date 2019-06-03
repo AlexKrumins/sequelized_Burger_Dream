3321d067635c7336b6907a422e5b0a62827bcd37
Alex <justkrumins@gmail.com>
2019-06-03 13:06:01 -0500

    fired button functions

--- public/assets/js/chefs.js
+++ public/assets/js/chefs.js
@@ -19,5 +19,21 @@ $(function() {
       alert("Please give your chef a really dreamy name.")
     }
   });
+
+  $(".fired").on("click", function(event) {
+    event.preventDefault();
+
+    var tableItemData = $(this).parent("td").parent("tr").data("author");
+    var id = tableItemData.id;
+    console.log(id);
+    $.ajax({
+      method: "DELETE",
+      url: "/api/authors/" + id
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
+  })
 });
  
