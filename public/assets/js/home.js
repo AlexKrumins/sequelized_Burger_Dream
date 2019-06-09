05d8953fb441559e9875eeac6d530212e374f8db
Alex Krumins <justkrumins@gmail.com>
2019-06-09 11:21:56 -0500

    routes for chefId

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,35 +1,49 @@
-$(function() {
-
-  $(".change-devour").on("click", function() {
-    event.preventDefault();
+$(document).ready(function() {
+  var url = window.location.search;
+  var burgerId;
+  var chefId
+  
+  $(function() {
     
-    var burger_id = $(this).data("burger_id");
-    var changeState = $(this).data("changestate");
-    var newState = {
-      id: burger_id,
-      devoured: changeState
-    };
-    $.ajax({
-      method: "PUT",
-      url: "/api/burgers/" + burger_id,
-      data: newState
-    }).then(
-      function() {
-        location.reload();
-      });
-  });
-
-  $(".baku-delete").on("click", function(event) {
-    event.preventDefault();
+    if (url.indexOf("?burger_id=") !== -1) {
+      burgerId = url.split("=")[1];
+      getPostData(burgerId, "post");
+    }
+    else if (url.indexOf("?chef_id=") !== -1) {
+      chefId = url.split("=")[1];
+    }
     
-    var id = $(this).data("burger_id");
-    console.log(id);
-    $.ajax({
-      method: "DELETE",
-      url: "/api/burgers/" + id
-    }).then(
-      function() {
-        location.reload();
-      });
+    $(".change-devour").on("click", function() {
+      event.preventDefault();
+      
+      var burger_id = $(this).data("burger_id");
+      var changeState = $(this).data("changestate");
+      var newState = {
+        id: burger_id,
+        devoured: changeState
+      };
+      $.ajax({
+        method: "PUT",
+        url: "/api/burgers/" + burger_id,
+        data: newState
+      }).then(
+        function() {
+          location.reload();
+        });
+    });
+
+    $(".baku-delete").on("click", function(event) {
+      event.preventDefault();
+      
+      var id = $(this).data("burger_id");
+      console.log(id);
+      $.ajax({
+        method: "DELETE",
+        url: "/api/burgers/" + id
+      }).then(
+        function() {
+          location.reload();
+        });
+    });
   });
 });
